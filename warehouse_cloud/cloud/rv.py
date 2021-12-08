7195ef68f42cf37d149360e9921556db93defd49
proseuche <proseuche@hanmail.net>
2021-12-08 14:51:03 +0900

    RV

--- warehouse_cloud/cloud/rv.py
+++ warehouse_cloud/cloud/rv.py
@@ -1,5 +1,24 @@
 import time
 
+from runtime_verification import event, scope, property
+
+globally_scope = scope.GloballyScope(None)
+
+inventory_over_event = event.InventoryOverEvent("Inventory Over")
+absence_property = property.Absence(inventory_over_event, globally_scope)
+
+item_type_valid_event = event.ItemTypeValidEvent("Item Type Valid")
+universality_property = property.Universality(item_type_valid_event, globally_scope)
+
+item_number_event = event.ItemNumberEvent("Item number 1", 1)
+existence_property = property.Existence(item_number_event, globally_scope)
+
+properties = [
+    absence_property,
+    universality_property,
+    existence_property
+]
+
 
 def run(seconds):
     time.sleep(seconds)
@@ -7,4 +26,8 @@ def run(seconds):
 
 
 def run_verification():
-    pass
+    for prop in properties:
+        prop.check()
+
+    for prop in properties:
+        print(prop.name, prop.status)
--- warehouse_cloud/runtime_verification/event.py
+++ warehouse_cloud/runtime_verification/event.py
@@ -1,6 +1,41 @@
+from cloud.models import Inventory
+
+
 class Event:
     def __init__(self, name):
         self.name = name
 
     def check_hold(self):
         return False
+
+class InventoryOverEvent(Event):
+    def check_hold(self):
+        for i in range(4):
+            items = Inventory.objects.filter(stored=i)
+            if len(items) > 5:
+                return True
+
+        return False
+
+class ItemTypeValidEvent(Event):
+    def check_hold(self):
+        items = Inventory.objects.filter(stored__lt=4)
+        for item in items:
+            if item.item_type not in [1, 2, 3, 4]:
+                return False
+
+        return True
+
+class ItemNumberEvent(Event):
+    def __init__(self, name, x):
+        super().__init__(name)
+        self.name = name
+        self.limit = x
+        self.past = False
+
+    def check_hold(self):
+        items = Inventory.objects.filter(stored__lt=4)
+        if len(items) >= self.limit:
+            return True
+
+        return False or self.past
